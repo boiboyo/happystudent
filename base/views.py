0b805ec8a285cd63c599675a0a38ceb0e732b7fb
boiboyo <johnboiboiyo123@gmai.com>
2022-10-04 14:48:27 +0300

    First Milestone

--- base/views.py
+++ base/views.py
@@ -7,13 +7,6 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, MyUserCreationForm
 
-# Create your views here.
-
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn python!'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend developers'},
-# ]
 
 
 def loginPage(request):
--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '10.1.52.166',
+    '0.0.0.0',
+]
 
 
 # Application definition
